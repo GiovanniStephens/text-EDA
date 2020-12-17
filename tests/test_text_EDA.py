0ebcd178ef5722c364af588f6656effdb5743e72
Giovanni Stephens <stephens.giovanni@gmail.com>
2020-12-18 11:43:36 +1300

    Added a test to ensure that the get_word_count works with SpaCy doc objects.

--- tests/test_text_EDA.py
+++ tests/test_text_EDA.py
@@ -12,17 +12,25 @@ class test_embeddings(unittest.TestCase):
         import importlib
         return importlib.import_module(module_name)
 
-    def test_get_word_count(self):
-        """Tests that we're getting the right word count."""
-        text_EDA = self._get_module('text_EDA')
-        self.assertEqual(text_EDA.get_word_count(TEST_UTTERANCES[0]), 5)
-
     def test_load_nlp_pipe(self):
         """Tests that the nlp pipe has been set up correctly."""
         text_EDA = self._get_module('text_EDA')
         explorer = text_EDA.text_EDA(TEST_UTTERANCES)
         self.assertEqual(explorer.nlp.pipe_names, \
              ['tagger', 'sentencizer', 'parser', 'ner', 'entity_ruler']) 
+
+    def test_get_word_count_str(self):
+        """Tests that we're getting the right word count from a string."""
+        text_EDA = self._get_module('text_EDA')
+        self.assertEqual(text_EDA.get_word_count(TEST_UTTERANCES[0]), 5)
+
+    def test_get_word_count(self):
+        """Tests running the data through SpaCy's NLP pipeline
+        and then getting the word count."""
+        text_EDA = self._get_module('text_EDA')
+        explorer = text_EDA.text_EDA(TEST_UTTERANCES)
+        self.assertEqual(text_EDA.get_word_count(\
+            explorer.data['Raw Utterances'].iloc[0]), 5)
 
 if __name__ == '__main__':
     unittest.main()
