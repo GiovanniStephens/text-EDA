eda6891984e89ff1d1af5fdf8f9fa4a0fe1f782c
Giovanni Stephens <stephens.giovanni@gmail.com>
2020-12-21 13:49:26 +1300

    Added sentiments to the pandas dataframe.

--- text_EDA.py
+++ text_EDA.py
@@ -148,3 +148,4 @@ class text_EDA():
         self.data['Word Densities'] = list(map(get_word_density, self.nlp_utterances))
         self.data['Punctuation Counts'] = list(map(get_punctuation_count, self.nlp_utterances))
         self.data['Case Ratios'] = list(map(get_case_ratio, self.nlp_utterances))
+        self.data['Sentiments'] = list(map(get_sentiment, self.nlp_utterances))
